e50a119def8fe77114d19185194dc56c5f8a5530
Zgloob <s214563@student.uek.krakow.pl>
2021-04-19 18:15:22 +0200

    2021-04-19 MZ

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-respons = requests.get('https://www.ceneo.pl/32622086#tab=reviews')
+def extractComponent(opinion, selector, attribute=None):
+    try:
+        if attribute:
+            return opinion.select(selector).pop(0)[attribute].strip()
+        if attribute is None:
+            return opinion.select(selector).pop(0).get_text().strip()      
+        
+        return [item.get_text().strip() for item in opinion.select(selector)] 
+    except IndexError:
+        return None
+
+
+productId = input("Podaj kod produktu: ")
+respons = requests.get('https://www.ceneo.pl/{}#tab=reviews'.format(productId))
 page = 2
 opinionsList = []
 
@@ -14,41 +27,45 @@ while respons:
 
     opinions = pageDOM.select("div.js_product-review")
 
-
     for opinion in opinions:
 
-
         opinionId = opinion["data-entry-id"]
-
-        author = opinion.select("span.user-post__author-name").pop(0).get_text().strip()
-        try:
-            rcmd = opinion.select("span.user-post__author-recomendation > em").pop(0).get_text().strip()
-            rcmd = True if rcmd == "Polecam" else False
-        except IndexError:
-            rcmd = None
-
-        stars = opinion.select("span.user-post__score-count").pop(0).get_text().strip()
+        author = extractComponent(opinion, "span.user-post__author-name")
+        rcmd = extractComponent(opinion,"span.user-post__author-recomendation > em")
+        stars = extractComponent(opinion,"span.user-post__score-count")
         stars= float(stars.split("/")[0].replace(",","."))
 
-        content = opinion.select("div.user-post__text").pop(0).get_text().strip()
+        content = extractComponent(opinion,"div.user-post__text")
         content.replace("\n"," ").replace("\r"," ")
-        pros = opinion.select("[class*=\"positives\"]~ div.review-feature_item")
-        pros = [item.get_text().strip() for item in pros]
-
-        cons = opinion.select("div[class*=\"negatives\"] ~ div.review-feature_item")
-        cons = [item.get_text().strip() for item in cons]
-        try: 
-            purchased = bool(opinion.select("div.review-pz").pop(0).get_text().strip())
-        except IndexError:
-            purchased = False
-        publishDate = opinion.select("span.user-post__published > time:nth-child(1)").pop(0)["datetime"].strip()
-        try:
-            purchaseDate = opinion.select("span.user-post__published > time:nth-child(2)").pop(0)["datetime"].strip()
+        ############################################
+       
+        pros = extractComponent(
+            opinion,"[class*=\"positives\"]~ div.review-feature_item", [])
+        cons = extractComponent(
+            opinion,"div[class*=\"negatives\"] ~ div.review-feature_item", [])
+                
+        
+        #########################
+        purchased = extractComponent(opinion,"div.review-pz")
+        #########################
+        publishDate = extractComponent(
+            opinion, "span.user-post__published > time:nth-child(1)", "datetime")    
+        purchaseDate = extractComponent(
+            opinion,"span.user-post__published > time:nth-child(2)", "datetime")
         except IndexError:
             purchaseDate = None
+        ########################################
+        useful = extractComponent(opinion,"span[id^='votes-yes']")
+        useless = extractComponent(opinion,"span[id^='votes-no']")
+        
+        rcmd = True if rcmd == "Polecam" else False
+        stars= float(stars.split("/")[0].replace(",","."))
+        content.replace("\n"," ").replace("\r"," ")
+        purchased = bool(purchased)
+        useful = int(useful)
+        useless = int(useless)
 
-        useful = int(opinion.select("span[id^='votes-yes']").pop(0).get_text().strip())
-        useless = int(opinion.select("span[id^='votes-no']").pop(0).get_text().strip())
+
         opinionDict = {
             "opinionId": opinionId,
             "author":author,
@@ -66,13 +83,13 @@ while respons:
         }
         opinionsList.append(opinionDict)
 
-    respons = requests.get('https://www.ceneo.pl/32622086/opinie-'+str(page), allow_redirects = False)
+    respons = requests.get('https://www.ceneo.pl/{}/opinie-'.format(productId)+str(page), allow_redirects = False)
     if respons.status_code==200:
         page += 1
     else:
         break
 
-    with open("./opinions/32622086.json", "w", encoding="UTF-8") as f:
+    with open(f"./opinions/{productId}.json", "w", encoding="UTF-8") as f:
         json.dump(opinionsList, f, indent=4, ensure_ascii=False)
 
 
